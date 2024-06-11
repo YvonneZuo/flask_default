82a5735d2dd8b8a5ec7c1031885b6eb7302c64a8
zuoyifang <zuo.yif@northeastern.edu>
2024-06-10 17:28:17 -0700

    load model in the app

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, jsonify
+import pandas as pd
+import numpy as np
+import joblib
+
+
 app = Flask(__name__)
 
+# Load the model using joblib
+model = joblib.load('model_pipeline.pkl')
+# Define the expected columns
+cols = ['credit_history', 'purpose', 'credit_amount', 'savings_account', 'personal_status_and_sex', 'job']
 
 
 @app.route('/')
@@ -9,10 +18,18 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    return render_template('home.html',pred='25%')
+    int_features = [x for x in request.form.values()]
+    final = np.array(int_features).reshape(1, -1)
+    data_unseen = pd.DataFrame(final, columns=cols)
+    prediction = model.predict(data_unseen)
+    return render_template('home.html', pred='Expected outcome is {}'.format(prediction[0]))
 
-
-
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    data = request.get_json(force=True)
+    data_unseen = pd.DataFrame([data])
+    prediction = model.predict(data_unseen)
+    return jsonify(prediction[0])
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=config.DEBUG_MODE)
+    app.run(host="0.0.0.0",port=config.PORT, debug=config.DEBUG_MODE)
--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 from os import environ as env
+
 DEBUG_MODE = int(env.get('DEBUG_MODE', 1))
+PORT = int(env.get("PORT", 8080))
